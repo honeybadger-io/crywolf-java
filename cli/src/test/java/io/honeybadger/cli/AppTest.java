ff75bdbb4a048e08f4f3256d07dbe78f028a6889
Elijah Zupancic <elijah@zupancic.name>
2015-08-29 17:10:36 -0700

    Added play framework example

--- cli/src/test/java/io/honeybadger/cli/AppTest.java
+++ cli/src/test/java/io/honeybadger/cli/AppTest.java
@@ -5,6 +5,14 @@ import org.junit.Test;
 public class AppTest {
     @Test(expected = java.lang.RuntimeException.class)
     public void appCanCallHoneybadgerWithoutError() {
-        App.main(new String[] { System.getProperty("honeybadger.api_key") } );
+        final String apiKey;
+
+        if (System.getProperty("honeybadger.api_key") != null) {
+            apiKey = System.getProperty("honeybadger.api_key");
+        } else {
+            apiKey = System.getenv("HONEYBADGER_API_KEY");
+        }
+
+        App.main(new String[] { apiKey });
     }
 }
---
+++ play/build.sbt
@@ -0,0 +1,21 @@
+name := """honeybadger-play"""
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayJava)
+
+scalaVersion := "2.11.6"
+
+resolvers += Resolver.mavenLocal
+
+libraryDependencies ++= Seq(
+  javaJdbc,
+  cache,
+  javaWs
+)
+
+libraryDependencies += "io.honeybadger" % "honeybadger-java" % "1.0.9"
+
+// Play provides two styles of routers, one expects its actions to be injected, the
+// other, legacy style, accesses its actions statically.
+routesGenerator := InjectedRoutesGenerator
